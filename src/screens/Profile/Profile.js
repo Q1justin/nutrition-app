35a39433dbe647f46d90c9cb5b3f250de4987709
Gyuwon Do <q1justindo@gmail.com>
2020-12-28 19:34:05 -0500

    Adding some styles for fun

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -4,9 +4,9 @@ import {  View, Text, Image, StyleSheet  } from 'react-native';
 class Profile extends Component {
   render(){
     return(
-      <View>
-        <View>
-          <Image source={require('../../assets/myFACE.jpg')}/>
+      <View style = {styles.screen}>
+        <View style = {styles.profileInfo}>
+          <Image source={require('../../assets/myFACE.jpg')} style = {styles.myFace}/>
           <Text>Name: Justin Do</Text>
           <Text>Age: 23</Text>
           <Text>Status: Hungry</Text>
@@ -16,5 +16,32 @@ class Profile extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+    screen: {
+      borderColor: 'black',
+      backgroundColor: '#85DB85',
+      width: 700,
+      borderBottomWidth:5,
+      borderTopWidth:5,
+      borderLeftWidth: 5,
+      borderRightWidth: 5,
+      marginHorizontal: 400,
+      marginVertical: 20,
+      height: 700
+    },
+    profileInfo: {
+        flex: 1,
+        alignItems: 'left',
+        justifyContent: "space-between",
+    },
+    myFace: {
+        borderRadius: 50,
+        width: 100,
+        height: 100,
+        marginHorizontal: 20,
+        marginVertical: 20,
+    }
+})
 
 export default Profile;
