0f59452360051c37a92aed7d5b3eb2939c375466
Gyuwon Do <q1justindo@gmail.com>
2020-12-28 21:09:29 -0500

    Added some random progress bars for stats

---
+++ src/components/Diary/Diary.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {  View, Text, StyleSheet  } from 'react-native';
+
+class Diary extends Component {
+  render(){
+    return(
+      <View>
+        <Text>This is where the diary goes</Text>
+      </View>
+    )
+  }
+}
+
+export default Diary;
--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import {  View, Text, Image, StyleSheet  } from 'react-native';
+import ProgressBar from 'react-native-progress/Bar';
+import Diary from '../../components/Diary/Diary';
 
 class Profile extends Component {
   render(){
     return(
       <View style = {styles.screen}>
-        <View style = {styles.profileInfo}>
+        <View style = {styles.profile}>
           <Image source={require('../../assets/myFACE.jpg')} style = {styles.myFace}/>
-          <Text>Name: Justin Do</Text>
-          <Text>Age: 23</Text>
-          <Text>Status: Hungry</Text>
-          <Text>Streak: 2</Text>
+          <View style = {styles.profileInfo}>
+            <Text style = {{fontWeight: 'bold', fontSize: 20, marginBottom:10}}>Justin Do</Text>
+            <Text style = {{fontSize: 15, marginBottom:10}}>Age: 23</Text>
+            <Text style = {{fontSize: 15, marginBottom:10}}>Status: Hungry</Text>
+            <Text style = {{fontSize: 15, marginBottom:10}}>Streak: 2</Text>
+          </View>
         </View>
+        <View style = {styles.stats}>
+          <Text>
+            Diligence
+          </Text>
+          <ProgressBar progress={0.8} width={350} />
+          <Text>
+            Guts
+          </Text>
+          <ProgressBar progress={0.5} width={350} color = {'red'}/>
+        </View>
+        <Diary />
       </View>
     )
   }
@@ -21,7 +36,7 @@ const styles = StyleSheet.create({
     screen: {
       borderColor: 'black',
       backgroundColor: '#85DB85',
-      width: 700,
+      width: 400,
       borderBottomWidth:5,
       borderTopWidth:5,
       borderLeftWidth: 5,
@@ -30,17 +45,26 @@ const styles = StyleSheet.create({
       marginVertical: 20,
       height: 700
     },
-    profileInfo: {
+    profile: {
         flex: 1,
         alignItems: 'left',
-        justifyContent: "space-between",
+        flexDirection: 'row',
+        height: 150
+    },
+    profileInfo: {
+      width: 200,
+      marginHorizontal: 20,
+      marginVertical: 20
     },
     myFace: {
         borderRadius: 50,
-        width: 100,
-        height: 100,
+        width: 150,
+        height: 150,
         marginHorizontal: 20,
         marginVertical: 20,
+    },
+    stats: {
+        flex: 1,
     }
 })
 
